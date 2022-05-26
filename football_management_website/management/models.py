68766d2801be70f799253399bdd088cedfa71bd8
DATDEV <datpt101201@gmail.com>
2022-05-27 02:06:30 +0700

    fix logc sign/login

--- football_management_website/management/models.py
+++ football_management_website/management/models.py
@@ -8,10 +8,5 @@ class Account(models.Model):
   password = models.CharField(max_length=100,null=False)
   role = models.CharField(max_length=10,null=False)
   
-  def __init__(self, username, password, role):
-    self.username = username
-    self.password = password
-    self.role = role
-        
   def __str__(self):
     return self.username

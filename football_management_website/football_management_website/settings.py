bbe70b994bb53025c034db35f468f4f07497458d
DATDEV <datpt101201@gmail.com>
2022-05-27 02:15:26 +0700

    fix problem

--- football_management_website/football_management_website/settings.py
+++ football_management_website/football_management_website/settings.py
@@ -24,7 +24,7 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7hv@#(jk#h#6@q949euf_2-dy%t+ej!&*agw307pfal8p1!@@7'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

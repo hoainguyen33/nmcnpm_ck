95cd93509fa4e52dbaef0e5d84785e8f472ba571
vucan1892000@gmail.com <vucan1892000@gmail.com>
2022-06-18 11:10:14 +0700

    add cau thu doi bong

--- football_management_website/management/migrations/0004_account_delete_user.py
+++ football_management_website/management/migrations/0004_account_delete_user.py
@@ -25,3 +25,18 @@ class Migration(migrations.Migration):
             name='User',
         ),
     ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Account',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('username', models.CharField(max_length=20, null=True, unique=True)),
+                ('password', models.CharField(max_length=100)),
+                ('role', models.CharField(max_length=10)),
+            ],
+        ),
+        migrations.DeleteModel(
+            name='User',
+        ),
+    ]
